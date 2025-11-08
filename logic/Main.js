4e394867bb8976cced0263613f43e4d6d7b74225
Cjobi4 <obigregory4@gmail.com>
2025-11-08 16:43:30 -0700

    Had to change from Mandarin to Cantonese

---
+++ logic/Main.js
@@ -0,0 +1,15 @@
+//============ CJ Session ==================
+const phrases = {
+    emergency : [ { en: "I need help", es: "Necesito ayuda", fr: "J'ai besoin d'aide",  },
+                { en: "Call the police", es: "Llame a la policía", fr: "Appelez la police", de: "Rufen Sie die Polizei", it: "Chiama la polizia", pt: "Chame a polícia" },
+                { en: "Call an ambulance", es: "Llame una ambulancia", fr: "Appelez une ambulance", de: "Rufen Sie einen Krankenwagen", it: "Chiama un'ambulanza", pt: "Chame uma ambulância" }
+                ],
+
+    medical: [
+
+
+
+
+
+    ],
+}
